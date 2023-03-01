8406d124d2707e0a70865c1e558bbc31fe8f5e41
Manu Halvagal <mshalvagal@users.noreply.github.com>
2023-03-01 21:38:55 +0100

    Create 2-6-church-numerals.scm

---
+++ 2-6-church-numerals.scm
@@ -0,0 +1,20 @@
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;(add-1 zero)
+;(add-1 (lambda (f) (lambda (x) x)))
+;(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+;(lambda (f) (lambda (x) (f x)))
+
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+;(add-1 one)
+;(lambda (f) (lambda (x) (f ((one f) x))))
+;(lambda (f) (lambda (x) (f ((lamdba (x) (f x)) x))))
+;(lambda (f) (lambda (x) (f (f x))))
+
+(define (add m n)
+  (lambda (f) (lambda (x) ((m f) ((n f) x)))))
